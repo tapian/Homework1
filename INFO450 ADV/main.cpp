114e4ffa2650f9fc553ab4d36c326ef2f8cc66e8
Nydia Tapia <tapian@vcu.edu>
2019-09-12 17:45:03 -0400

    Initial Commit

---
+++ INFO450 ADV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  INFO450 ADV
+//
+//  Created by Nydia Tapia on 9/12/19.
+//  Copyright © 2019 Nydia Tapia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
